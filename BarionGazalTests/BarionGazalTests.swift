6f405988994070de343fe33bd6cc4e5a94c7a56f
Gabriel Gazal <gabrielrgrocha14@gmail.com>
2020-03-03 15:56:01 -0300

    Initial

---
+++ BarionGazalTests/BarionGazalTests.swift
@@ -0,0 +1,34 @@
+//
+//  BarionGazalTests.swift
+//  BarionGazalTests
+//
+//  Created by Gabriel Gazal on 03/03/20.
+//  Copyright © 2020 Gabriel Gazal. All rights reserved.
+//
+
+import XCTest
+@testable import BarionGazal
+
+class BarionGazalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
