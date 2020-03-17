505a7f604673b5f9ea0831068779b7b22d8005e3
Gabriel Gazal <gabrielrgrocha14@gmail.com>
2020-03-17 16:17:15 -0300

    ADmobs instaldo 2

--- BarionGazal/Controller/AppDelegate.swift
+++ BarionGazal/Controller/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Firebase
-
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
        
         return true
     }
